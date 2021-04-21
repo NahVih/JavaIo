03b1da044af062de88f921cb938494d2725cf426
NahVih <61999704+NahVih@users.noreply.github.com>
2021-04-21 11:31:37 +1000

    21.04
    21.04

---
+++ src/br/com/alura/java/io/test/ReadingCSV.java
@@ -0,0 +1,43 @@
+package br.com.alura.java.io.test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+
+public class ReadingCSV {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner scanner = new Scanner(new File("contas.csv"));
+		
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+//			System.out.println(line);
+			
+			Scanner lineScanner = new Scanner(line);
+			lineScanner.useLocale(Locale.US);
+			lineScanner.useDelimiter(",");
+			
+			String account = lineScanner.next();
+			int value2 = lineScanner.nextInt();
+			int number = lineScanner.nextInt();
+			String costumer = lineScanner.next();
+			double balance = lineScanner.nextDouble();
+			
+			String valueFormated = String.format("Informations: %s %04d-%06d %s %08.2f ", account, value2, number, costumer, balance);
+			System.out.println(valueFormated);
+			
+			System.out.println("-------------------");
+			
+			lineScanner.close();
+		}
+		
+	
+		
+		scanner.close();
+	}
+
+}
